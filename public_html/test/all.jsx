db9c2e1755ab2f6858e97ce097d02dc524b64420
Kayne Ruse <kayne@qpsbenchmarking.com>
2017-02-22 16:40:50 +1100

    I need to flesh this out

--- public_html/test/all.jsx
+++ public_html/test/all.jsx
@@ -1,9 +1,16 @@
-
+window.console.log = function(msg) { alert(msg); };
 
 describe("describe name", function() {
   it("it name", function() {
     var rootNode = document.createElement("DIV");
     rootNode.setAttribute("id", "root");
-    console.log("MARK 1");
+
+    document.querySelector('body').appendChild(rootNode);
+
+    console.log(document.querySelector('body'));
+
+    console.log(require('../src/app.jsx'));
+
+    console.log(document.querySelector('#root'));
   });
 });
